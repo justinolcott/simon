1808ea429476e9095c2295981a1dfd2d3697c40e
justinolcott <69178047+justinolcott@users.noreply.github.com>
2023-03-06 16:06:43 -0700

    added scores.js

---
+++ scores.js
@@ -0,0 +1,33 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+        scores = JSON.parse(scoresText);
+    }
+
+    const tableBodyEl = document.querySelector('#scores');
+
+    if (scores.length) {
+        for (cosnt [f, score] of scores.entries()) {
+            const positionTdEl = document.createElement('td');
+            const nameTdEl = document.createElement('td');
+            const scoreTdEl = document.createElement('td');
+            const dateTdEl = document.createElement('td');
+
+            positionTdEl.textContent = i + 1;
+            nameTdEl.textContent = score.name;
+            scoreTdEl.textContent = score.score;
+            dateTdEl.textContent = score.data;
+
+            const rowEl = document.createElement('tr');
+            rowEl.appendChild(positionTdEl);
+            rowEl.appendChild(nameTdEl);
+            rowEl.appendChild(scoreTdEl);
+            rowEl.appendChild(dateTdEl);
+
+            tableBodyEl.appendChild(rowEl);
+        }
+    } else {
+        tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+}
